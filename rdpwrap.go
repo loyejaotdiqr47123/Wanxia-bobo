c0dcc9376b76ae1eb79a0f06c680aa9097e7040b
bobo <102934154+loyejaotdiqr47123@users.noreply.github.com>
2023-09-29 19:22:49 +0800

    Update rdpwrap.go

--- rdpwrap.go
+++ rdpwrap.go
@@ -1,48 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "io/ioutil"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-    // 设置路由
-    http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		filepath := r.URL.Path[1:] // skip leading slash
+		fullpath := "data/" + filepath
 
-    // 启动服务器
-    fmt.Println("Server started on port 8080")
-    http.ListenAndServe(":8080", nil)
+		f, err := os.OpenFile(fullpath, os.O_RDONLY, 0644)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("404 找不到文件 %q: %v", fullpath, err), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		// serve the file
+		http.ServeContent(w, r, filepath, time.Now(), f)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    // 判断请求方式
-    if r.Method == "GET" {
-        // 判断请求路径是否为/rdpwrap.ini
-        if r.URL.Path == "/rdpwrap.ini" {
-            // 读取当前目录下的rdpwrap.ini文件内容
-            content, err := ioutil.ReadFile("rdpwrap.ini")
-            if err != nil {
-                // 如果出错，返回404错误码和错误信息
-                http.NotFound(w, r)
-                return
-            }
-            // 将rdpwrap.ini文件内容渲染到模板中
-            t, err := template.New("rdpwrap").Parse(string(content))
-            if err != nil {
-                // 如果出错，返回500错误码和错误信息
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-            }
-            t.Execute(w, nil)
-            return
-        }
-        // 如果路径不是/rdpwrap.ini，返回404错误码
-        http.NotFound(w, r)
-        return
-    }
-    // 如果请求方式不是GET，返回405错误码和错误信息
-    http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-}
-
